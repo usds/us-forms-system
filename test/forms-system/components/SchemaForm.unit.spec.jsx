992b3f1de763da6f4d8eda97402c20fa4fb1f727
Kainic <vacokainia@VACOLTKAINIA1.fios-router.home>
2018-05-24 13:30:27 -0400

    Set up initial unit test infastructure

---
+++ test/forms-system/components/SchemaForm.unit.spec.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { expect } from 'chai';
+import SkinDeep from 'skin-deep';
+import sinon from 'sinon';
+
+import SchemaForm from '../../../src/js/forms-system/components/SchemaForm';
+
+describe('Schemaform <SchemaForm>', () => {
+  it('should render', () => {
+    const name = 'testPage';
+    const schema = {};
+    const uiSchema = {};
+    const data = {};
+
+    const tree = SkinDeep.shallowRender(
+      <SchemaForm
+        name={name}
+        title={name}
+        schema={schema}
+        uiSchema={uiSchema}
+        pageData={data}/>
+    );
+
+    expect(tree.everySubTree('Form')).not.to.be.empty;
+  });
+});
