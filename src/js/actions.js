04e43f2aad228784a4f369e818733a1d9808225a
Dave Methvin <David.W.Methvin@omb.eop.gov>
2018-09-11 12:49:45 -0400

    Last set of typos

--- src/js/actions.js
+++ src/js/actions.js
@@ -134,7 +134,7 @@ function submitToUrl(body, submitUrl, recordEvent) {
 
 export function submitForm(formConfig, form) {
   const recordEvent = formConfig.recordEvent ?
-    formConfig.recordEvent(formConfig) :
+    formConfig.recordEvent :
     console.log.bind(console);      // eslint-disable-line no-console
 
   return dispatch => {
--- src/js/review/SubmitController.jsx
+++ src/js/review/SubmitController.jsx
@@ -74,7 +74,7 @@ class SubmitController extends React.Component {
     const { isValid, errors } = isValidForm(form, pagesByChapter);
     if (!isValid) {
       const recordEvent = formConfig.recordEvent ?
-        formConfig.recordEvent(formConfig) :
+        formConfig.recordEvent :
         console.log.bind(console);   // eslint-disable-line no-console
 
       recordEvent({ event: 'validation-failed-on-submit', errors });
