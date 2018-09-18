873884d132d02b25ae8b3cc9749f56ae0dd1be1c
Dave Methvin <David.W.Methvin@omb.eop.gov>
2018-09-17 21:38:15 -0400

    Add examples directory

---
+++ docs/examples/CMS40B-E--form.js
@@ -0,0 +1,129 @@
+import Introduction from '../components/Introduction.jsx';
+
+const formConfig = {
+  title: 'Application for enrollment in Medicare Part B (medical insurance)',
+  subTitle: 'CMS40B-E',
+  formId: '',
+  urlPrefix: '/',
+  submitUrl: '',
+  introduction: Introduction,
+  confirmation: '',
+  defaultDefinitions: {},
+  preSubmitInfo: {
+    notice: 'I hope you did not lie!',
+    required: true,
+    label: 'I swear this information is correct to the best of my knowledge.',
+    field: 'signed',
+    error: 'You must check the box to submit this form and apply for Medicare Part B'
+  },
+  chapters: {
+    CP1: {
+      pages: {
+        PG1: {
+          path: 'verification',
+          title: 'Enrollment verification',
+          uiSchema: {
+            signUp: {
+              'ui:title': 'I wish to apply for Medicare Part B',
+              'ui:options': {
+                expandUnderClassNames: 'schemaform-expandUnder-indent'
+              }
+            },
+            'view:preQualification': {
+              'ui:description': 'To apply for Medicare Part B, you must have already applied for and recieved Medicare Part A. If you do not have Part A and want to sign up, please contact Social Security at 1-800-772-1213. TTY users should call 1-800-325-0778.',
+              'ui:options': {
+                expandUnder: 'signUp'
+              }
+            },
+            medicareNumber: {
+              'ui:title': 'Medicare Number',
+              'ui:description': 'Your Part A paperwork has a Medicare number in the form XXXX-XXX-XXXX consisting only of letters and digits, please enter it below.',
+              'ui:errorMessages': {
+                pattern: 'Your Medicare number should be in the form XXXX-XXX-XXXX, letters and digits separated by dashes'
+              }
+            }
+          },
+          schema: {
+            type: 'object',
+            required: [ 'signUp', 'medicareNumber' ],
+            properties: {
+              signUp: {
+                type: 'boolean'
+              },
+              'view:preQualification': {
+                type: 'object',
+                properties: {}
+              },
+              medicareNumber: {
+                type: 'string',
+                pattern: '[0-9a-zA-z]{4}-[0-9a-zA-z]{3}-[0-9a-zA-z]{4}'
+              }
+            }
+          }
+        },
+        PG2: {
+          path: 'enrollee-info',
+          title: 'Enrollee information',
+          uiSchema: {
+            zip: {
+              'ui:errorMessages': {
+                pattern: 'Please enter a 5-digit zip code or 9-digit zip+4 code'
+              }
+            },
+            phone: {
+              'ui:errorMessages': {
+                minLength: 'Please enter a phone number, including area code'
+              }
+            }
+          },
+          schema: {
+            type: 'object',
+            required: [ 'name', 'address', 'city', 'state', 'zip', 'phone' ],
+            properties: {
+              name: {
+                type: 'string',
+                title: 'Full name (Last, First, Middle)',
+                minLength: 5
+              },
+              address: {
+                type: 'string',
+                title: 'Address',
+                minLength: 5
+              },
+              city: {
+                type: 'string',
+                title: 'City',
+                minLength: 2
+              },
+              'state': {
+                  type: 'string',
+                  title: 'State',
+                  'enum': [
+                    'AL', 'AK', 'AS', 'AZ', 'AR', 'AA', 'AE', 'AP', 'CA', 'CO',
+                    'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL',
+                    'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI',
+                    'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY',
+                    'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI',
+                    'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV',
+                    'WI', 'WY'
+                  ]
+              },
+              zip: {
+                type: 'string',
+                title: 'Zip Code',
+                pattern: '[0-9]{5}((-)?[0-9]{4})?'
+              },
+              phone: {
+                type: 'string',
+                title: 'Phone',
+                minLength: 10
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+export default formConfig;
---
+++ docs/examples/README.md
@@ -0,0 +1,30 @@
+# Form.js examples
+
+The `form.js` file configures most of the form inputs, pages, chapters, and settings in a project. In the [starter app](https://github.com/usds/us-forms-system-starter-app/), this file is stored [in the config directory](https://github.com/usds/us-forms-system-starter-app/tree/3617106dfea7c42f5c397f96f24fb906c8e2879d/js/config).
+
+To use one of the example files in this directory, rename it to `form.js` and replace the file that is in the locally installed version of the starter app. Online versions of the forms are also available on [codesandbox.io](https://codesandbox.io) that can be used for experimentation. The examples available are listed below.
+
+## CMS40B-E: Appication for Enrollment in Medicare Part B
+* [Original PDF form](https://www.cms.gov/Medicare/CMS-Forms/CMS-Forms/Downloads/CMS40B-E.pdf)
+* [form.js file](./CMS40B-E--form.js)
+* [codesandbox.io form using the starter app]()
+
+## DS-82: U.S. Passport Renewal Application for Eligible Individuals
+* [Original PDF form](https://eforms.state.gov/Forms/ds82.pdf)
+* [form.js file](./DS-82--form.js)
+* [codesandbox.io form using the starter app]()
+
+## SS-5: Application for Social Security Card
+* [Original PDF form]((https://www.ssa.gov/forms/ss-5.pdf)
+* [form.js file](./SS-5--form.js)
+* [codesandbox.io form using the starter app]()
+
+## G-1145
+* [Original PDF form]()
+* [form.js file]()
+* [codesandbox.io form using the starter app]()
+
+## 4506-T
+* [Original PDF form]()
+* [form.js file]()
+* [codesandbox.io form using the starter app]()
