f8dfcbdf6548527bd6e1a83efac58360ee339b16
Dave Methvin <David.W.Methvin@omb.eop.gov>
2018-09-28 13:44:36 -0400

    Allow year definition to include future years
    Fixes #221

--- src/js/definitions/year.js
+++ src/js/definitions/year.js
@@ -1,10 +1,10 @@
-import { validateCurrentOrPastYear } from '../validation';
 import * as ReviewWidget from '../review/widgets';
 
 function schema() {
   return {
     type: 'integer',
-    minimum: 1900
+    minimum: 1900,
+    maximum: 2100
   };
 }
 
@@ -12,12 +12,6 @@ function uiSchema(title = 'Year') {
   return {
     'ui:title': title,
     'ui:reviewWidget': ReviewWidget.TextWidget,
-    'ui:validations': [
-      validateCurrentOrPastYear
-    ],
-    'ui:errorMessages': {
-      type: 'Please provide a valid year'
-    },
     'ui:options': {
       widgetClassNames: 'usa-input-medium'
     }
