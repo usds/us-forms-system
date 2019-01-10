2b44328ec265a17f78d150ff07fce57a96e437b9
Bill John Tran <bjtran1234@gmail.com>
2019-01-10 08:44:29 -0800

    add Required to ErrorableCheckbox (#329)
    * add Required to ErrorableCheckbox
    
    * update tests for ErrorableCheckbox
    
    * run build

--- src/js/components/ErrorableCheckbox.jsx
+++ src/js/components/ErrorableCheckbox.jsx
@@ -33,7 +33,7 @@ class ErrorableCheckbox extends React.Component {
     // Calculate required.
     let requiredSpan = undefined;
     if (this.props.required) {
-      requiredSpan = <span className="form-required-span">*</span>;
+      requiredSpan = <span className="form-required-span">(*Required)</span>;
     }
 
     let className = `form-checkbox${
