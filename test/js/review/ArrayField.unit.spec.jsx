eef2ded11b5e3d4870b79fc7da67ada1007c53b6
Rian Fowler <rian.fowler@adhocteam.us>
2019-02-14 10:56:36 -0700

    fix unit test

--- test/js/review/ArrayField.unit.spec.jsx
+++ test/js/review/ArrayField.unit.spec.jsx
@@ -282,11 +282,11 @@ describe('Schemaform review <ArrayField>', () => {
       expect(tree.everySubTree('SchemaForm').length).to.equal(2);
     });
     it('enforces max items', () => {
-      expect(tree.subTree('button').props.disabled).to.be.false;
+      expect(tree.subTree('.edit-btn').props.disabled).to.be.false;
 
       tree.getMountedInstance().handleAdd();
 
-      expect(tree.subTree('button').props.disabled).to.be.true;
+      expect(tree.subTree('.edit-btn').props.disabled).to.be.true;
     });
     it('remove', () => {
       expect(tree.everySubTree('SchemaForm').length).to.equal(1);
